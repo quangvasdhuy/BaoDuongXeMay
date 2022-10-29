ccb7a06e57be27f08f5702142e5b23ba23d9db1f
tkdudat2 <tkdudat2@gmail.com>
2022-10-29 11:32:07 +0700

    update flutter 29/10/2022

---
+++ FE/baoduongxe_app/lib/services/base_client.dart
@@ -0,0 +1,235 @@
+import 'dart:convert';
+
+import 'package:baoduongxe_app/model/accessary.dart';
+import 'package:baoduongxe_app/model/vehicle.dart';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+import '../model/user.dart';
+
+const String baseUrl = 'https://localhost:44337/api';
+
+class BaseClient {
+  var client = http.Client();
+
+  //GET
+  Future<dynamic> get(String api) async {
+    var url = Uri.parse(baseUrl + api);
+    var _headers = {
+      'Authorization': 'Bearer sfie328370428387=',
+      'api_key': 'ief873fj38uf38uf83u839898989',
+    };
+
+    var response = await client.get(url, headers: _headers);
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      //throw exception and catch it in UI
+      throw Exception('Request API error!');
+    }
+  }
+  //POST
+  Future<dynamic> post(String api, dynamic object) async {
+    var url = Uri.parse(baseUrl + api);
+    var _payload = json.encode(object);
+    var _headers = {
+      'Authorization': 'Bearer sfie328370428387=',
+      'Content-Type': 'application/json',
+      'api_key': 'ief873fj38uf38uf83u839898989',
+    };
+
+    var response = await client.post(url, body: _payload, headers: _headers);
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      //throw exception and catch it in UI
+      throw Exception('Request API error!');
+    }
+  }
+
+  //PUT Request
+  Future<dynamic> put(String api, dynamic object) async {
+    var url = Uri.parse(baseUrl + api);
+    var _payload = json.encode(object);
+    var _headers = {
+      'Authorization': 'Bearer sfie328370428387=',
+      'Content-Type': 'application/json',
+      'api_key': 'ief873fj38uf38uf83u839898989',
+    };
+
+    var response = await client.put(url, body: _payload, headers: _headers);
+    if (response.statusCode == 204) {
+      return response.body;
+    } else {
+      //throw exception and catch it in UI
+      throw Exception('Request API error!');
+    }
+  }
+
+  //DELETE
+  Future<dynamic> delete(String api) async {
+    var url = Uri.parse(baseUrl + api);
+    var _headers = {
+      'Authorization': 'Bearer sfie328370428387=',
+      'api_key': 'ief873fj38uf38uf83u839898989',
+    };
+
+    var response = await client.delete(url, headers: _headers);
+    if (response.statusCode == 204) {
+      return response.body;
+    } else {
+      //throw exception and catch it in UI
+      throw Exception('Request API error!');
+    }
+  }
+
+  void login(String api, dynamic object) async {
+    var url = Uri.parse(baseUrl + api);
+    var _payload = json.encode(object);
+    var _headers = {
+      'Authorization': 'Bearer sfie328370428387=',
+      'Content-Type': 'application/json',
+      'api_key': 'ief873fj38uf38uf83u839898989',
+    };
+
+    var response = await client.post(url, body: _payload, headers: _headers);
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body.toString());
+      print(data['token']);
+      print('Login success!');
+    } else {
+      //throw exception and catch it in UI
+      throw Exception('Request API error!');
+    }
+  }
+
+  Future<List<User>> fetchUsers(String api) async{
+    var url = Uri.parse(baseUrl + api);
+    var _headers = {
+      'Authorization': 'Bearer sfie328370428387=',
+      'api_key': 'ief873fj38uf38uf83u839898989',
+    };
+    var response = await client.get(url, headers: _headers);
+    if (response.statusCode == 200) {
+      return userFromJson(response.body);
+    } else {
+      //throw exception and catch it in UI
+      throw Exception('Request API error!');
+    }
+  }
+
+  Future<List<Vehicle>> fetchVehicles(String api) async{
+    var url = Uri.parse(baseUrl + api);
+    var _headers = {
+      'Authorization': 'Bearer sfie328370428387=',
+      'api_key': 'ief873fj38uf38uf83u839898989',
+    };
+    var response = await client.get(url, headers: _headers);
+    if (response.statusCode == 200) {
+      return vehicleFromJson(response.body);
+    } else {
+      //throw exception and catch it in UI
+      throw Exception('Request API error!');
+    }
+  }
+
+  Future<List<Accessary>> fetchAccessaries(String api) async{
+    var url = Uri.parse(baseUrl + api);
+    var _headers = {
+      'Authorization': 'Bearer sfie328370428387=',
+      'api_key': 'ief873fj38uf38uf83u839898989',
+    };
+    var response = await client.get(url, headers: _headers);
+    if (response.statusCode == 200) {
+      return accessaryFromJson(response.body);
+    } else {
+      //throw exception and catch it in UI
+      throw Exception('Request API error!');
+    }
+  }
+
+
+  Future<List> getPosts(String api) async {
+    try {
+      var response = await http.get(Uri.parse(baseUrl + api));
+      if (response.statusCode == 200) {
+        // return a decoded body
+        return jsonDecode(response.body);
+      } else {
+        // server error
+        return Future.error("Server Error !");
+      }
+    } catch (SocketException) {
+      // fetching error
+      // may be timeout, no internet or dns not resolved
+      return Future.error("Error Fetching Data !");
+    }
+  }
+
+  Future<String> createPost(Map<String, dynamic> data,String api) async {
+    try {
+      var response = await http.post(
+        Uri.parse(baseUrl + api),
+        body: jsonEncode(data),
+        headers: {
+          "Content-Type": "application/json; charset=UTF-8",
+        },
+      );
+      if (response.statusCode == 200) {
+        return "success";
+      } else {
+        print(response.body);
+        // server error
+        return "err";
+      }
+    } catch (SocketException) {
+      // fetching error
+      return "err";
+    }
+  }
+
+  Future<String> editPost(Map<String, dynamic> data,String api) async {
+    try {
+      var response = await http.put(
+        Uri.parse(baseUrl + api),
+        body: jsonEncode(data),
+        headers: {
+          "Content-Type": "application/json; charset=UTF-8",
+        },
+      );
+      if (response.statusCode == 204) {
+        return "success";
+      } else {
+        print(response.body);
+        // server error
+        return "err";
+      }
+    } catch (SocketException) {
+      // fetching error
+      return "err";
+    }
+  }
+
+  Future<String> deletePost(Map<String, dynamic> data, String api) async {
+    try {
+      var response = await http.delete(
+        Uri.parse(baseUrl + api),
+        body: jsonEncode(data),
+        headers: {
+          "Content-Type": "application/json; charset=UTF-8",
+        },
+      );
+      //
+      if (response.statusCode == 204) {
+        return "success";
+      } else {
+        print(response.body);
+        // server error
+        return "err";
+      }
+    } catch (SocketException) {
+      // fetching error
+      return "err";
+    }
+  }
+}
---
+++ FE/baoduongxe_app/lib/ui/register.dart
@@ -0,0 +1,233 @@
+import 'package:flutter/material.dart';
+import '../model/user.dart';
+import '../services/base_client.dart';
+
+class MyRegister extends StatefulWidget {
+  const MyRegister({Key? key}) : super(key: key);
+
+  @override
+  _MyRegisterState createState() => _MyRegisterState();
+}
+
+class _MyRegisterState extends State<MyRegister> {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('assets/register.png'), fit: BoxFit.cover),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: Stack(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(left: 35, top: 30),
+              child: const Text(
+                'Create\nAccount',
+                style: TextStyle(color: Colors.white, fontSize: 33),
+              ),
+            ),
+            SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.28),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(left: 35, right: 35),
+                      child: Column(
+                        children: [
+                          TextField(
+                            controller: nameController,
+                            style: const TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(40),
+                                  borderSide: const BorderSide(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(40),
+                                  borderSide: const BorderSide(
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                hintText: "Name",
+                                hintStyle: const TextStyle(color: Colors.white),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(40),
+                                )),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          TextField(
+                            controller: usernameController,
+                            style: const TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(40),
+                                  borderSide: const BorderSide(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(40),
+                                  borderSide: const BorderSide(
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                hintText: "User Name",
+                                hintStyle: const TextStyle(color: Colors.white),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(40),
+                                )),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          TextField(
+                            controller: emailController,
+                            style: const TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(40),
+                                  borderSide: const BorderSide(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(40),
+                                  borderSide: const BorderSide(
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                hintText: "Email",
+                                hintStyle: const TextStyle(color: Colors.white),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(40),
+                                )),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          TextField(
+                            controller: passwordController,
+                            style: const TextStyle(color: Colors.white),
+                            obscureText: true,
+                            decoration: InputDecoration(
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(40),
+                                  borderSide: const BorderSide(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(40),
+                                  borderSide: const BorderSide(
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                hintText: "Password",
+                                hintStyle: const TextStyle(color: Colors.white),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(40),
+                                )),
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                'Sign Up =>',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 27,
+                                    fontWeight: FontWeight.w700),
+                              ),
+                              CircleAvatar(
+                                radius: 28,
+                                backgroundColor: Colors.cyanAccent.shade400,
+                                child: IconButton(
+                                    color: Colors.white,
+                                    onPressed: () async {
+                                      var user = User(
+                                          name: nameController.text.toString(),
+                                          username: usernameController.text.toString(),
+                                          email: emailController.text.toString(),
+                                          password: passwordController.text.toString());
+                                      var response = await BaseClient()
+                                          .post('/Users', user)
+                                          .catchError((err) {});
+                                      if (response == null) return;
+                                      debugPrint('Register success:');
+                                      showDialog(context: context, builder: (context){
+                                        return AlertDialog(
+                                          title: const Text("Register ?"),
+                                          actions: [
+                                            TextButton(onPressed: (){
+                                              Navigator.pop(context);
+                                            }, child: const Text("NO",style: TextStyle(color: Colors.red),)),
+                                            TextButton(onPressed: (){
+                                              Navigator.pushNamed(context, 'login');
+                                            }, child: const Text("YES"))
+                                          ],
+                                        );
+                                      });
+                                    },
+                                    icon: const Icon(
+                                      Icons.add,
+                                      color: Colors.white,
+                                    )),
+                              )
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 28,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pushNamed(context, 'login');
+                                },
+                                style: const ButtonStyle(),
+                                child: const Text(
+                                  'Sign In',
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                      decoration: TextDecoration.underline,
+                                      color: Colors.white,
+                                      fontSize: 18),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
